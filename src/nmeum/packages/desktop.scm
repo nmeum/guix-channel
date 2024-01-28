f1c155bda7bc456d58e01dc84f8ef3ecf0adaab4
Sören Tempel <soeren+git@soeren-tempel.net>
2024-01-28 09:24:04 +0100

    nmeum: creek: upgrade to 0.2.0

--- src/nmeum/packages/desktop.scm
+++ src/nmeum/packages/desktop.scm
@@ -19,37 +19,36 @@
       (modify-inputs (package-native-inputs fcft)
                      (prepend utf8proc)))))
 
-(define-public levee
-  (let ((commit "d9f908e54f2905c2a01b4dd7d3091dd70d09298f"))
-    (package
-      (name "levee")
-      (version (git-version "20240120" "0" commit))
-      (source
-        (origin
-          (method git-fetch)
-          (uri (git-reference
-                 (url "https://github.com/nmeum/levee.git")
-                 (commit commit)
-                 (recursive? #t)))
-          (file-name (git-file-name name version))
-          (sha256
-            (base32 "16xm0li67zc2ihjblcfqfkzfklspyf0y1b79f4vxsjf2k3x5wi3j"))))
-      (build-system zig-build-system)
-      (arguments
-        (list
-          #:zig-release-type "safe"
-          #:tests? #f))
-      (native-inputs
-        (list
-          pixman
-          fcft-utf8
-          pkg-config
-          wayland
-          wayland-protocols
-          zig))
-      (home-page "https://github.com/nmeum/leeve")
-      (synopsis "A minimalistic and malleable status bar for the River compositor.")
-      (description "A fork of the levee status bar which intended to be
+(define-public creek
+  (package
+    (name "creek")
+    (version "0.2.0")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/nmeum/creek")
+               (commit (string-append "v" version))
+               (recursive? #t)))
+        (file-name (git-file-name name version))
+        (sha256
+          (base32 "08v8f39wxpalifib0760nkckl1xbbxqcjcmv68348w9b8367kzx6"))))
+    (build-system zig-build-system)
+    (arguments
+      (list
+        #:zig-release-type "safe"
+        #:tests? #f))
+    (native-inputs
+      (list
+        pixman
+        fcft-utf8
+        pkg-config
+        wayland
+        wayland-protocols
+        zig))
+    (home-page "https://github.com/nmeum/leeve")
+    (synopsis "A minimalistic and malleable status bar for the River compositor.")
+    (description "A fork of the creek status bar which intended to be
 more mallable then the original version.  For example,
 supporting custom status bar information via stdin.")
-      (license license:expat))))
+    (license license:expat)))
