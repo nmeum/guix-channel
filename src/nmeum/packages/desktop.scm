ebf166a5e2241cdb09af75f6b9fdbf713672f1d9
Sören Tempel <soeren+git@soeren-tempel.net>
2024-02-12 20:31:52 +0100

    nmeum: Add dumb-runtime-dir.

--- src/nmeum/packages/desktop.scm
+++ src/nmeum/packages/desktop.scm
@@ -2,10 +2,12 @@
   #:use-module (guix)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
   #:use-module (guix build-system zig)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages zig)
   #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages fontutils)
@@ -18,6 +20,41 @@
     (native-inputs
       (modify-inputs (package-native-inputs fcft)
                      (prepend utf8proc)))))
+
+(define-public dumb-runtime-dir
+  (package
+    (name "dumb-runtime-dir")
+    (version "1.0.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/ifreund/dumb_runtime_dir")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0nrxhvbh3bs4pi4f5h03zw1p1ys19qmmlx263ysly8302wkxk1m4"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      #:make-flags #~(list (string-append "CC="
+                                          #$(cc-for-target))
+                           (string-append "PREFIX=")
+                           (string-append "DESTDIR="
+                                          #$output))
+      #:phases #~(modify-phases %standard-phases
+                   (delete 'configure))))
+    (inputs (list linux-pam))
+    (native-inputs (list pkg-config))
+    (home-page "https://github.com/ifreund/dumb_runtime_dir")
+    (synopsis "Creates an XDG_RUNTIME_DIR via PAM")
+    (description
+     "Provides a acronym{PAM, Pluggable Authentication Module} for
+creating a primitve @code{XDG_RUNTIME_DIR} that is never removed.  This
+is useful in conjuction with simple seat managers like @code{seatd}
+which do not set @code{XDG_RUNTIME_DIR}.")
+    (license license:bsd-0)))
 
 (define-public creek
   (package
