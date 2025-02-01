7f2e77c69f0b0620fd3422d46088c9f9dc4a59cc
Sören Tempel <soeren+git@soeren-tempel.net>
2025-02-01 11:48:35 +0100

    networking: Switch to new dhcpcd service
    From https://issues.guix.gnu.org/75934

--- src/nmeum/services/networking.scm
+++ src/nmeum/services/networking.scm
@@ -15,49 +15,104 @@
             dhcpcd-configuration
             dhcpcd-configuration?
             dhcpcd-configuration-interfaces
-            dhcpcd-configuration-options))
+            dhcpcd-configuration-command-args
+            dhcpcd-configuration-hostname
+            dhcpcd-configuration-duid
+            dhcpcd-configuration-persistent
+            dhcpcd-configuration-option
+            dhcpcd-configuration-require
+            dhcpcd-configuration-slaac
+            dhcpcd-configuration-nooption
+            dhcpcd-configuration-nohook
+            dhcpcd-configuration-static
+            dhcpcd-configuration-vendorclassid
+            dhcpcd-configuration-clientid
+            dhcpcd-configuration-extra-content))
 
 ;;
-;; dhcpcd
+;; DHCPCD.
 ;;
 
-(define-maybe list)
+(define (dhcpcd-serialize-string field-name value)
+  (let ((field (object->string field-name)))
+    (if (string=? field "extra-content")
+      #~(string-append #$value "\n")
+      #~(format #f "~a ~a~%" #$field #$value))))
 
-;; Ensure that strings within the unbound configuration
-;; are not enclosed in double quotes by the serialization.
-(define (->string obj)
-  (if (string? obj)
-    obj
-    (object->string obj)))
+(define (dhcpcd-serialize-boolean field-name value)
+  (if value
+    #~(format #f "~a~%" #$(object->string field-name))
+    ""))
 
-(define (serialize-list-of-opts field-name lst)
-  #~(string-append
-      (string-join
-        (list
-          #$@(map
-               (lambda (lst)
-                 (string-join (map ->string lst) " "))
-               lst)) "\n") "\n"))
+(define (dhcpcd-serialize-list-of-strings field-name value)
+  #~(string-append #$@(map (cut dhcpcd-serialize-string field-name <>) value)))
 
-(define (list-of-opts? lst)
-  (list? lst))
+;; Some fields (e.g. hostname) can be specified with an empty string argument.
+;; Therefore, we need a maybe type to differentiate disabled/empty-string.
+(define-maybe string (prefix dhcpcd-))
 
 (define-configuration dhcpcd-configuration
   (interfaces
-    maybe-list
+    (list '())
     "List of interfaces to start a DHCP client for."
     empty-serializer)
-  (options
-    ;; Replicate the default dhcpcd configuration file.
-    ;; See: https://github.com/NetworkConfiguration/dhcpcd#configuration
-    (list-of-opts '((hostname)
-                    (duid)
-                    (persistent)
-                    (option rapid_commit)
-                    (option interface_mtu)
-                    (require dhcp_server_identifier)
-                    (slaac private)))
-    "List of configuration options for dhcpcd."))
+  (command-args
+    (list '("-q" "-q"))
+    "List of additional command-line options."
+    empty-serializer)
+
+  ;; The following defaults replicate the default dhcpcd configuration file.
+  ;;
+  ;; See https://github.com/NetworkConfiguration/dhcpcd/tree/v10.0.10#configuration
+  (hostname
+    (maybe-string "")
+    "Hostname to send via DHCP, defaults to the current system hostname.")
+  (duid
+    (maybe-string "")
+    "Use and generate a DHCP Unique Identifier.")
+  (persistent
+    (boolean #t)
+    "Do not de-configure on shutdown.")
+  (option
+    (list-of-strings
+      '("rapid_commit"
+        "domain_name_servers"
+        "domain_name"
+        "domain_search"
+        "host_name"
+        "classless_static_routes"
+        "interface_mtu"))
+    "List of options to request from the server.")
+  (require
+    (list-of-strings '("dhcp_server_identifier"))
+    "List of options to require in responses.")
+  (slaac
+    (maybe-string "private")
+    "Interface identifier used for SLAAC generated IPv6 addresses.")
+
+  ;; Common options not set in the default configuration file.
+  (nooption
+    (list-of-strings '())
+    "List of options to remove from the message before it's processed.")
+  (nohook
+    (list-of-strings '())
+    "List of hook script which should not be invoked.")
+  (static
+    (list-of-strings '())
+    "Configure a static value (e.g. ip_address).")
+  (vendorclassid
+    maybe-string
+    "Set the DHCP Vendor Class.")
+  (clientid
+    maybe-string
+    "Use the interface hardware address or the given string as a Client ID.")
+
+  ;; Escape hatch for the generated configuration file.
+  (extra-content
+    maybe-string
+    "Extra content to append to the configuration as-is.")
+
+  (prefix dhcpcd-))
 
 (define (dhcpcd-config-file config)
   (mixed-text-file "dhcpcd.conf"
@@ -73,41 +128,22 @@
           (system? #t)
           (comment "dhcpcd daemon user")
           (home-directory "/var/empty")
-          (shell "/run/current-system/profile/sbin/nologin"))))
+          (shell (file-append shadow "/sbin/nologin")))))
 
 (define (dhcpcd-shepherd-service config)
   (let* ((config-file (dhcpcd-config-file config))
-         (interfaces (dhcpcd-configuration-interfaces config)))
+         (command-args (dhcpcd-configuration-command-args config))
+         (ifaces (dhcpcd-configuration-interfaces config)))
     (list (shepherd-service
-            (documentation "dhcp daemon.")
+            (documentation "dhcpcd daemon.")
             (provision '(networking))
             (requirement '(user-processes udev))
             (actions (list (shepherd-configuration-action config-file)))
-            (start #~(lambda _
-                       ;; When invoked without any arguments, the client discovers all
-                       ;; non-loopback interfaces *that are up*.  However, the relevant
-                       ;; interfaces are typically down at this point.  Thus we perform
-                       ;; our own interface discovery here.
-                       ;;
-                       ;; Taken from the `dhcp-client-shepherd-service`.
-                       (define valid?
-                         (lambda (interface)
-                           (and (arp-network-interface? interface)
-                                (not (loopback-network-interface? interface))
-                                ;; XXX: Make sure the interfaces are up so that
-                                ;; 'dhclient' can actually send/receive over them.
-                                ;; Ignore those that cannot be activated.
-                                (false-if-exception
-                                  (set-network-interface-up interface)))))
-                       (define ifaces
-                         (filter valid?
-                                 #$(if (maybe-value-set? interfaces)
-                                     #~'#$interfaces
-                                     #~(all-network-interface-names))))
-
-                       (fork+exec-command
-                         (cons* (string-append #$dhcpcd "/sbin/dhcpcd")
-                                "-q" "-q" "-B" "-f" #$config-file ifaces))))
+            (start
+              #~(lambda _
+                  (fork+exec-command
+                    (list (string-append #$dhcpcd "/sbin/dhcpcd")
+                          #$@command-args "-B" "-f" #$config-file #$@ifaces))))
             (stop #~(make-kill-destructor))))))
 
 (define dhcpcd-service-type
