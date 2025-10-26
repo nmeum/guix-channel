3b8d75af2cf0374b224259144d1f167af8fbbc1f
Sören Tempel <soeren+git@soeren-tempel.net>
2025-10-26 11:58:04 +0100

    desktop: sort alphabetically

--- src/nmeum/packages/desktop.scm
+++ src/nmeum/packages/desktop.scm
@@ -17,6 +17,71 @@
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages textutils))
 
+(define-public creek
+  (package
+    (name "creek")
+    (version "0.4.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/nmeum/creek")
+             (commit (string-append "v" version))
+             (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0b1gpxgi207cqrls6qp0pd6749g60yfcrjii8nagm00c678bs3nx"))))
+    (build-system zig-build-system)
+    (arguments
+     (list
+      #:zig-release-type "safe"
+      #:tests? #f))
+    (native-inputs (list fcft
+                         pixman
+                         pkg-config
+                         zig-wayland
+                         zig-fcft
+                         zig-pixman))
+    (home-page "https://github.com/nmeum/leeve")
+    (synopsis
+     "A minimalistic and malleable status bar for the River compositor.")
+    (description "A fork of the creek status bar which intended to be
+more mallable then the original version.  For example,
+supporting custom status bar information via stdin.")
+    (license license:expat)))
+
+(define-public dam
+  (let ((commit "e6eb713fb3239aad3c534502d8c0e42e4e514c8f")
+        (revision "1"))
+    (package
+      (name "dam")
+      (version (git-version "0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://codeberg.org/sewn/dam")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0ybhyvv3n1xdy5hb1xw7l2rzy73n5vhh8r60gfbf61crhggnzh8k"))))
+      (build-system gnu-build-system)
+      (arguments
+       (list
+        #:tests? #f
+        #:make-flags #~(list (string-append "PREFIX=" #$output))
+        #:phases #~(modify-phases %standard-phases
+                     (add-before 'configure 'set-cc-command
+                       (lambda _
+                         (setenv "CC" #$(cc-for-target))))
+                     (delete 'configure))))
+      (inputs (list fcft pixman wayland wayland-protocols))
+      (native-inputs (list pkg-config))
+      (home-page "https://codeberg.org/sewn/dam")
+      (synopsis "A itsy-bitsy dwm-esque bar for the river compositor")
+      (description "")
+      (license license:expat))))
+
 (define-public dumb-runtime-dir
   (package
     (name "dumb-runtime-dir")
@@ -52,63 +117,19 @@ is useful in conjuction with simple seat managers like @code{seatd}
 which do not set @code{XDG_RUNTIME_DIR}.")
     (license license:bsd-0)))
 
-(define-public zig-fcft
+(define-public font-terminus-patched
   (package
-    (name "zig-fcft")
-    (version "1.1.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://git.sr.ht/~novakane/zig-fcft")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0im0rdwww7xxvhmibfp3459h8wwdmihjb9m7vmzxkgqs4x77l114"))))
-    (build-system zig-build-system)
+    (inherit font-terminus)
+    (name "font-terminus-patched")
     (arguments
-     (list
-      #:skip-build? #t
-      #:tests? #f))
-    (propagated-inputs (list zig-pixman fcft))
-    (native-inputs (list pkg-config))
-    (synopsis "Zig bindings for the fcft font library")
-    (description "")
-    (home-page "https://git.sr.ht/~novakane/zig-fcft")
-    (license license:expat)))
-
-(define-public creek
-  (package
-    (name "creek")
-    (version "0.4.2")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/nmeum/creek")
-             (commit (string-append "v" version))
-             (recursive? #t)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0b1gpxgi207cqrls6qp0pd6749g60yfcrjii8nagm00c678bs3nx"))))
-    (build-system zig-build-system)
-    (arguments
-     (list
-      #:zig-release-type "safe"
-      #:tests? #f))
-    (native-inputs (list fcft
-                         pixman
-                         pkg-config
-                         zig-wayland
-                         zig-fcft
-                         zig-pixman))
-    (home-page "https://github.com/nmeum/leeve")
-    (synopsis
-     "A minimalistic and malleable status bar for the River compositor.")
-    (description "A fork of the creek status bar which intended to be
-more mallable then the original version.  For example,
-supporting custom status bar information via stdin.")
-    (license license:expat)))
+     (substitute-keyword-arguments (package-arguments font-terminus)
+       ((#:phases phases
+         '%standard-phases)
+        #~(modify-phases #$phases
+            (add-after 'unpack 'patch-font
+              (lambda _
+                (invoke "patch" "-p0" "-i" "alt/td1.diff")
+                (invoke "patch" "-p0" "-i" "alt/ll2.diff")))))))))
 
 (define-public ustatus
   (let ((commit "4032e2010de049472b7af57e85b7e4728fdde5e7")
@@ -191,48 +212,27 @@ supporting custom status bar information via stdin.")
       (description "")
       (license license:wtfpl2))))
 
-(define-public dam
-  (let ((commit "e6eb713fb3239aad3c534502d8c0e42e4e514c8f")
-        (revision "1"))
-    (package
-      (name "dam")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://codeberg.org/sewn/dam")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0ybhyvv3n1xdy5hb1xw7l2rzy73n5vhh8r60gfbf61crhggnzh8k"))))
-      (build-system gnu-build-system)
-      (arguments
-       (list
-        #:tests? #f
-        #:make-flags #~(list (string-append "PREFIX=" #$output))
-        #:phases #~(modify-phases %standard-phases
-                     (add-before 'configure 'set-cc-command
-                       (lambda _
-                         (setenv "CC" #$(cc-for-target))))
-                     (delete 'configure))))
-      (inputs (list fcft pixman wayland wayland-protocols))
-      (native-inputs (list pkg-config))
-      (home-page "https://codeberg.org/sewn/dam")
-      (synopsis "A itsy-bitsy dwm-esque bar for the river compositor")
-      (description "")
-      (license license:expat))))
-
-(define-public font-terminus-patched
+(define-public zig-fcft
   (package
-    (inherit font-terminus)
-    (name "font-terminus-patched")
+    (name "zig-fcft")
+    (version "1.1.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.sr.ht/~novakane/zig-fcft")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0im0rdwww7xxvhmibfp3459h8wwdmihjb9m7vmzxkgqs4x77l114"))))
+    (build-system zig-build-system)
     (arguments
-     (substitute-keyword-arguments (package-arguments font-terminus)
-       ((#:phases phases
-         '%standard-phases)
-        #~(modify-phases #$phases
-            (add-after 'unpack 'patch-font
-              (lambda _
-                (invoke "patch" "-p0" "-i" "alt/td1.diff")
-                (invoke "patch" "-p0" "-i" "alt/ll2.diff")))))))))
+     (list
+      #:skip-build? #t
+      #:tests? #f))
+    (propagated-inputs (list zig-pixman fcft))
+    (native-inputs (list pkg-config))
+    (synopsis "Zig bindings for the fcft font library")
+    (description "")
+    (home-page "https://git.sr.ht/~novakane/zig-fcft")
+    (license license:expat)))
