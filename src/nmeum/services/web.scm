db3aa68033952b4ab2c668087ec8ff3c0dd318c1
Sören Tempel <soeren+git@soeren-tempel.net>
2026-04-27 20:15:10 +0200

    Add radicale-gunicorn-service-type.
    See also: https://codeberg.org/guix/guix/issues/8185

--- src/nmeum/services/web.scm
+++ src/nmeum/services/web.scm
@@ -2,9 +2,12 @@
   #:use-module (nmeum packages misc)
   #:use-module (guix gexp)
   #:use-module (gnu packages admin)
+  #:use-module (gnu packages dav)
   #:use-module (gnu services)
   #:use-module (gnu services base)
+  #:use-module (gnu services mail)
   #:use-module (gnu services shepherd)
+  #:use-module (gnu services web)
   #:use-module (gnu system accounts)
   #:use-module (gnu system shadow)
   #:use-module (srfi srfi-1)
@@ -43,3 +46,41 @@
                                            webdav-server-shepherd-service)))
                 (compose concatenate)
                 (default-value '())))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; XXX: This is hopefully a temporary hack until we come up with a better way
+;; of using gunicorn-service-type with existing WSGI application services.
+;;
+;; See <https://codeberg.org/guix/guix/issues/8185>
+
+(define serialize-radicale-configuration
+  (@@ (gnu services mail) serialize-radicale-configuration))
+
+(define radicale-accounts
+  (@@ (gnu services mail) %radicale-accounts))
+
+(define radicale-activation
+  (@@ (gnu services mail) radicale-activation))
+
+(define-public radicale-gunicorn-service-type
+  (service-type
+   (name 'radicale-gunicorn)
+   (description "Run Radicale via the gunicorn WSGI server.")
+   (extensions
+    (list (service-extension
+            gunicorn-service-type
+            (lambda (conf)
+              (let ((cfg (serialize-radicale-configuration conf)))
+                (list (gunicorn-app
+                        (name "radicale")
+                        (package radicale)
+                        (wsgi-app-module "radicale")
+                        ;;(sockets '("127.0.0.1:8080"))
+                        (user "radicale")
+                        (group "radicale")
+                        (environment-variables
+                          (list (cons "RADICALE_CONFIG" cfg))))))))
+          (service-extension account-service-type (const radicale-accounts))
+          (service-extension activation-service-type radicale-activation)))
+   (default-value (radicale-configuration))))
